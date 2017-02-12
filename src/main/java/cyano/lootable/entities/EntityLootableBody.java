606b9a74fb41e3052e79d6e4f59576f1ba5ef7a4
TheCodedOne <TheCodedOne@users.noreply.github.com>
2017-02-12 05:28:55 +0000

    Update EntityLootableBody.java

--- src/main/java/cyano/lootable/entities/EntityLootableBody.java
+++ src/main/java/cyano/lootable/entities/EntityLootableBody.java
@@ -613,7 +613,7 @@ public class EntityLootableBody extends EntityLiving implements IInventory{
 
 	private static boolean matchesAny(String damageType, DamageSource... list){
 		for(int i = 0; i < list.length; i++){
-			if (damageType.equalsIgnoreCase(list[i].damageType)) return true;
+			if(damageType.equalsIgnoreCase(list[i].damageType)) return true;
 		}
 		return false;
 	}
