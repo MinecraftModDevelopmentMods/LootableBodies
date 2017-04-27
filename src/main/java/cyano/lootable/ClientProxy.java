cd6d6052c4b19be43b500819725eff3e549cdc0e
Jasmine Iwanek <jasmine@iwanek.co.uk>
2017-04-27 12:10:22 -0700

    reformat source

--- src/main/java/cyano/lootable/ClientProxy.java
+++ src/main/java/cyano/lootable/ClientProxy.java
@@ -9,31 +9,32 @@ import net.minecraftforge.fml.common.event.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.RenderManager;
 
-public class ClientProxy extends Proxy{
-    @Override
-    public void preInit(FMLPreInitializationEvent e) {
-        super.preInit(e);
-        // client-only pre-init code
-    }
+public class ClientProxy extends Proxy {
+	@Override
+	public void preInit(FMLPreInitializationEvent e) {
+		super.preInit(e);
+		// client-only pre-init code
+	}
 
-    @Override
-    public void init(FMLInitializationEvent e) {
-        super.init(e);
-        // client-only init code
-        RenderManager rm = Minecraft.getMinecraft().getRenderManager();
-        // add renderers
-        if(LootableBodies.fancyCorpses){
-        	RenderingRegistry.registerEntityRenderingHandler(EntityLootableBody.class, new RenderSkinnedLootableBody(rm));
-        }else {
-        	RenderingRegistry.registerEntityRenderingHandler(EntityLootableBody.class, new RenderLootableBody(rm));
-        }
-    	
-    }
+	@Override
+	public void init(FMLInitializationEvent e) {
+		super.init(e);
+		// client-only init code
+		RenderManager rm = Minecraft.getMinecraft().getRenderManager();
+		// add renderers
+		if (LootableBodies.fancyCorpses) {
+			RenderingRegistry.registerEntityRenderingHandler(EntityLootableBody.class,
+					new RenderSkinnedLootableBody(rm));
+		} else {
+			RenderingRegistry.registerEntityRenderingHandler(EntityLootableBody.class, new RenderLootableBody(rm));
+		}
 
-    @Override
-    public void postInit(FMLPostInitializationEvent e) {
-        super.postInit(e);
-        // client-only post-init code
-    }
-    
+	}
+
+	@Override
+	public void postInit(FMLPostInitializationEvent e) {
+		super.postInit(e);
+		// client-only post-init code
+	}
+
 }
--- src/main/java/cyano/lootable/graphics/RenderLootableBody.java
+++ src/main/java/cyano/lootable/graphics/RenderLootableBody.java
@@ -13,51 +13,50 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-public class RenderLootableBody extends RenderBiped{
+public class RenderLootableBody extends RenderBiped {
 
-    private static final ResourceLocation skeletonTexture = new ResourceLocation("textures/entity/skeleton/skeleton.png");
-    
+	private static final ResourceLocation skeletonTexture = new ResourceLocation(
+			"textures/entity/skeleton/skeleton.png");
+
 	public RenderLootableBody(RenderManager rm) {
 		super(rm, new ModelCorpseSkeleton(), 0.5f);
-        this.addLayer(new LayerHeldItem(this));
-        this.addLayer(new LayerBipedArmor((RendererLivingEntity)this) {
-            @Override
-            protected void initArmor() {
-                this.field_177189_c = new ModelCorpseSkeleton(0.5f, true); // model base (child version?)
-                this.field_177186_d = new ModelCorpseSkeleton(1.0f, true); // model base
-            }
-        });
-        
-        
-    }
-    
-	
-	
-    
-//    
-//    @Override
-//    public void func_82422_c() {
-//        GlStateManager.translate(0.09375f, 0.1875f, 0.0f);
-//    }
-//    
-	
-    @Override protected void preRenderCallback(final EntityLivingBase entity, final float unknown) {
-    	//EntityLootableBody e = (EntityLootableBody)entity;
-    	GlStateManager.color(0.8f, 0.9f, 0.9f);
-    	GlStateManager.rotate(90f, 1f, 0f, 0f);
-    	GlStateManager.translate(0f, 1f, 0.125f);
-    }
-    
-    private ResourceLocation getTexture(final EntityLootableBody e) {
-        return skeletonTexture;
-    }
-    
-    @Override
-    protected ResourceLocation getEntityTexture(final EntityLiving e){
-        return this.getTexture((EntityLootableBody)e);
-    }
-    
-    
-   
-    
+		this.addLayer(new LayerHeldItem(this));
+		this.addLayer(new LayerBipedArmor((RendererLivingEntity) this) {
+			@Override
+			protected void initArmor() {
+				this.field_177189_c = new ModelCorpseSkeleton(0.5f, true); // model
+																			// base
+																			// (child
+																			// version?)
+				this.field_177186_d = new ModelCorpseSkeleton(1.0f, true); // model
+																			// base
+			}
+		});
+
+	}
+
+	//
+	// @Override
+	// public void func_82422_c() {
+	// GlStateManager.translate(0.09375f, 0.1875f, 0.0f);
+	// }
+	//
+
+	@Override
+	protected void preRenderCallback(final EntityLivingBase entity, final float unknown) {
+		// EntityLootableBody e = (EntityLootableBody)entity;
+		GlStateManager.color(0.8f, 0.9f, 0.9f);
+		GlStateManager.rotate(90f, 1f, 0f, 0f);
+		GlStateManager.translate(0f, 1f, 0.125f);
+	}
+
+	private ResourceLocation getTexture(final EntityLootableBody e) {
+		return skeletonTexture;
+	}
+
+	@Override
+	protected ResourceLocation getEntityTexture(final EntityLiving e) {
+		return this.getTexture((EntityLootableBody) e);
+	}
+
 }
