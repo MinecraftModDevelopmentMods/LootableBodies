ff79f69e1f48485a7b829178aab28b51d13333cf
Jasmine Iwanek <jasmine@iwanek.co.uk>
2018-02-10 03:27:05 -0800

    Push for 1.11.2

--- src/main/java/cyano/lootable/LootableBodies.java
+++ src/main/java/cyano/lootable/LootableBodies.java
@@ -22,7 +22,7 @@ import net.minecraftforge.fml.common.registry.EntityRegistry;
 public class LootableBodies {
     public static final String MODID = "lootablebodies";
     public static final String NAME ="DrCyano's Lootable Bodies";
-    public static final String VERSION = "2.2.0";
+    public static final String VERSION = "2.3.0";
 	private static LootableBodies INSTANCE = null;
 
 	public static boolean displayNameTag = true;
