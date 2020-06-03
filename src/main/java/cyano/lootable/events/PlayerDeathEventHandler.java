38febe33979aef57f790c767b6b468f83601f1ba
Jasmine Iwanek <jasmine@iwanek.co.uk>
2020-06-03 14:02:27 -0700

    Fix cornercase dupe bug courtesy of GoldSloth's assistance

--- src/main/java/cyano/lootable/events/PlayerDeathEventHandler.java
+++ src/main/java/cyano/lootable/events/PlayerDeathEventHandler.java
@@ -1,79 +1,81 @@
 package cyano.lootable.events;
 
-import net.minecraft.enchantment.EnchantmentHelper;
+import java.util.Map;
+
 import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cyano.lootable.LootableBodies;
+import cyano.lootable.entities.EntityLootableBody;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
-import net.minecraftforge.event.entity.player.PlayerDropsEvent;
-import cyano.lootable.LootableBodies;
-import cyano.lootable.entities.EntityLootableBody;
-
-import java.util.Map;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 
 public class PlayerDeathEventHandler {
 
 	// set PlayerDropsEvent to *LOWEST* priority to remove compatibility issues
 	@SubscribeEvent(priority = EventPriority.LOWEST)
-	public void playerDeathEvent(PlayerDropsEvent event) {
+	public void playerDeathEvent(LivingDeathEvent event) {
 		// define variables here
-		EntityPlayer player = (EntityPlayer) event.entity;
-		World w = event.entity.worldObj;
-		// checks to see if event isn't canceled before continuing, preventing
-		// errors
-		if ((!player.worldObj.isRemote) && (!event.isCanceled()))
+		if (event.entity instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) event.entity;
+			World w = event.entity.worldObj;
+			// checks to see if event isn't canceled before continuing, preventing
+			// errors
+			if ((!player.worldObj.isRemote) && (!event.isCanceled()))
 			;
 
-		// initiates body creation after passing check
-		{
-			float rotation = player.getRotationYawHead();
-			EntityLootableBody corpse = new EntityLootableBody(w);
-			corpse.setPositionAndRotation(player.posX, player.posY, player.posZ, rotation, 0);
-			corpse.setDeathTime(w.getTotalWorldTime());
-			// System.out.println("Creating corpse with UUID
-			// "+corpse.getOwner()+" at
-			// ("+corpse.posX+","+corpse.posY+","+corpse.posZ+") with rotation
-			// "+rotation+".");
-			if (w.getGameRules().getGameRuleBooleanValue("keepInventory") == false) {
-				// set items
-				corpse.setCurrentItemOrArmor(0, EntityLootableBody.applyItemDamage(withdrawHeldItem(player)));
-				for (int i = 0; i < 4; i++) {
-					Map<Integer, Integer> enchantments = player.inventory.armorInventory[i] != null
+			// initiates body creation after passing check
+			{
+				float rotation = player.getRotationYawHead();
+				EntityLootableBody corpse = new EntityLootableBody(w);
+				corpse.setPositionAndRotation(player.posX, player.posY, player.posZ, rotation, 0);
+				corpse.setDeathTime(w.getTotalWorldTime());
+				// System.out.println("Creating corpse with UUID
+				// "+corpse.getOwner()+" at
+				// ("+corpse.posX+","+corpse.posY+","+corpse.posZ+") with rotation
+				// "+rotation+".");
+				if (w.getGameRules().getGameRuleBooleanValue("keepInventory") == false) {
+					// set items
+					corpse.setCurrentItemOrArmor(0, EntityLootableBody.applyItemDamage(withdrawHeldItem(player)));
+					for (int i = 0; i < 4; i++) {
+						Map<Integer, Integer> enchantments = player.inventory.armorInventory[i] != null
 							? EnchantmentHelper.getEnchantments(player.inventory.armorInventory[i]) : null;
-					if (enchantments != null && enchantments.containsKey(LootableBodies.eioSoulboundID)) {
-						System.out.println("Armor item: This item should not be in the corpse");
-						// Skip
-					} else {
-						corpse.setCurrentItemOrArmor(i + 1,
+						if (enchantments != null && enchantments.containsKey(LootableBodies.eioSoulboundID)) {
+							System.out.println("Armor item: This item should not be in the corpse");
+							// Skip
+						} else {
+							corpse.setCurrentItemOrArmor(i + 1,
 								EntityLootableBody.applyItemDamage(player.getCurrentArmor(i)));
-						player.inventory.armorInventory[i] = null;
+							player.inventory.armorInventory[i] = null;
+						}
+					}
+					for (int i = 0; i < player.inventory.mainInventory.length; i++) {
+						Map<Integer, Integer> enchantments = player.inventory.mainInventory[i] != null
+								? EnchantmentHelper.getEnchantments(player.inventory.mainInventory[i]) : null;
+						if (enchantments != null && enchantments.containsKey(LootableBodies.eioSoulboundID)) {
+							System.out.println("Inventory item: This item should not be in the corpse");
+							// Skip
+						} else {
+							corpse.vacuumItem(player.inventory.mainInventory[i]);
+							player.inventory.mainInventory[i] = null;
+						}
 					}
 				}
-				for (int i = 0; i < player.inventory.mainInventory.length; i++) {
-					Map<Integer, Integer> enchantments = player.inventory.mainInventory[i] != null
-							? EnchantmentHelper.getEnchantments(player.inventory.mainInventory[i]) : null;
-					if (enchantments != null && enchantments.containsKey(LootableBodies.eioSoulboundID)) {
-						System.out.println("Inventory item: This item should not be in the corpse");
-						// Skip
-					} else {
-						corpse.vacuumItem(player.inventory.mainInventory[i]);
-						player.inventory.mainInventory[i] = null;
-					}
-				}
-			}
-			// for the LOLs
-			if (LootableBodies.addBonesToCorpse) {
+				// for the LOLs
+				if (LootableBodies.addBonesToCorpse) {
 				corpse.vacuumItem(new ItemStack(Items.rotten_flesh, w.rand.nextInt(3) + 1));
 				corpse.vacuumItem(new ItemStack(Items.bone, w.rand.nextInt(3) + 1));
+				}
+				// removed profile fetching as it isn't needed, and creates issues
+				// when creating bodies(duplicating them if Witchery is installed,
+				// and player is vampire*)
+				w.spawnEntityInWorld(corpse);
+				corpse.setRotation(rotation);
+				// }
 			}
-			// removed profile fetching as it isn't needed, and creates issues
-			// when creating bodies(duplicating them if Witchery is installed,
-			// and player is vampire*)
-			w.spawnEntityInWorld(corpse);
-			corpse.setRotation(rotation);
-			// }
 		}
 	}
 
